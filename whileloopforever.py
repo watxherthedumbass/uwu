87bae1d201c69542e34ea59a0315a2cfea23b620
watxherthedumbass <104895198+watxherthedumbass@users.noreply.github.com>
2022-05-04 17:52:30 +0800

    Create whileloopforever.py
    just for fun yes

---
+++ whileloopforever.py
@@ -0,0 +1,9 @@
+"Keeps printing im gay forever"
+While True:
+ print("im gay")
+
+"The famous hello world code where every programmer started"
+print("hello world")
+
+
+#im gonna add more stuff here hehehehe
